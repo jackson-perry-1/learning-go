6c122ad830aa912a8952c56e723c56b86f61809f
Jackson Perry <jacksonperry12345@gmail.com>
2026-05-11 12:39:49 -0600

    tutorial(2): Return greetings for multiple people

--- 2_create_a_module/hello/hello.go
+++ 2_create_a_module/hello/hello.go
@@ -11,12 +11,13 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Jackson")
+	names := []string{"Jackson", "Bob", "Sally"}
+	messages, err := greetings.Hellos(names)
 
 	// if there was an error, print it and exit
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
