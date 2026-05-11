0c8dc2d1a2c650717c1995649289f0b0c96ba8a4
Jackson Perry <jacksonperry12345@gmail.com>
2026-05-10 21:21:06 -0600

    tutorial(2): Return and handle an error

--- 2_create_a_module/greetings/greetings.go
+++ 2_create_a_module/greetings/greetings.go
@@ -1,8 +1,15 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// return an error if the name string is empty
+	if name == "" {
+		return "", errors.New("name was empty")
+	}
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- 2_create_a_module/hello/hello.go
+++ 2_create_a_module/hello/hello.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/jackson-perry-1/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Jackson")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// message, err := greetings.Hello("Jackson")
+	message, err := greetings.Hello("")
+
+	// if there was an error, print it and exit
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
