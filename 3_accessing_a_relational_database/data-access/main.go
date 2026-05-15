3408353be5665193cdc5c27eae891d4de63ec71b
Jackson Perry <jacksonperry12345@gmail.com>
2026-05-15 06:37:27 -0600

    tutorial(3): single row DB query

--- 3_accessing_a_relational_database/data-access/main.go
+++ 3_accessing_a_relational_database/data-access/main.go
@@ -45,6 +45,12 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Albums found: %v\n", albums)
+
+	alb, err := albumByID(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", alb)
 }
 
 func albumsByArtist(artist string) ([]Album, error) {
@@ -69,4 +75,17 @@ func albumsByArtist(artist string) ([]Album, error) {
 		return nil, fmt.Errorf("albumsByArtist %q: %v", artist, err)
 	}
 	return albums, nil
+}
+
+func albumByID(id int64) (Album, error) {
+	var alb Album
+
+	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumsById %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	}
+	return alb, nil
 }
